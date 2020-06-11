95cfbff8d82a2098d4783497fbebd6138c631e6f
Alan Ktquez <ktquez@gmail.com>
2020-06-11 08:24:08 -0300

    docs(darkmode): Add default props to metaThemeColor

--- config/defaultThemeConfig.js
+++ config/defaultThemeConfig.js
@@ -2,7 +2,12 @@ export const colorMode = {
   enabled: true,
   visible: true,
   props: {
-    modes: ['light', 'dark', 'system', 'sepia']
+    modes: ['light', 'dark', 'system', 'sepia'],
+    metaThemeColor: {
+      light: '#f2f2f2',
+      dark: '#232b32',
+      sepia: '#d8c7a2'
+    }
   }
 }
 
